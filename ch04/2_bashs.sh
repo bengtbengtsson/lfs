43d9c5311be4971f49b7a57ddd8c5d4b0923b625
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2025-03-24 19:46:15 +0100

    add exports in .bashrc

--- ch04/2_bashs.sh
+++ ch04/2_bashs.sh
@@ -9,9 +9,9 @@ cat > ~/.bashrc << "EOF"
 echo "Setting up bashrc"
 set +h
 umask 022
-LFS=/mnt/lfs
+export LFS=/mnt/lfs
 LC_ALL=POSIX
-LFS_TGT=$(uname -m)-lfs-linux-gnu
+export LFS_TGT=$(uname -m)-lfs-linux-gnu
 PATH=/usr/bin
 if [ ! -L /bin ]; then PATH=/bin:$PATH; fi
 PATH=$LFS/tools/bin:$PATH
