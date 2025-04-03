d78cefd2ebf89f5ac8d14d7418167a8c7cecfb1e
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2025-04-03 13:42:47 +0200

    fix bug in flit-core script

--- ch08/scripts/flit-core.sh
+++ ch08/scripts/flit-core.sh
@@ -5,8 +5,8 @@ echo "### Entering /sources"
 pushd /sources
 
 echo "### Extracting flit-core"
-tar -xf flit-core-3.11.0.tar.gz
-cd flit-core-3.11.0
+tar -xf flit_core-3.11.0.tar.gz
+cd flit_core-3.11.0
 
 echo "### Building flit-core wheel"
 pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
@@ -15,5 +15,5 @@ echo "### Installing flit-core from local wheel"
 pip3 install --no-index --find-links dist flit_core
 
 cd ..
-rm -rf flit-core-3.11.0
+rm -rf flit_core-3.11.0
 popd
