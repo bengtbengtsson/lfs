bfbde89f54a4161615bb4d9386c2a7c2e694cf77
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2025-04-03 14:07:20 +0200

    fix error in ch08 markupsafemscript

--- ch08/scripts/markupsafe.sh
+++ ch08/scripts/markupsafe.sh
@@ -5,8 +5,8 @@ echo "### Entering /sources"
 pushd /sources
 
 echo "### Extracting MarkupSafe"
-tar -xf MarkupSafe-3.0.2.tar.gz
-cd MarkupSafe-3.0.2
+tar -xf markupsafe-3.0.2.tar.gz
+cd markupsafe-3.0.2
 
 echo "### Building MarkupSafe wheel"
 pip3 wheel -w dist --no-cache-dir --no-build-isolation --no-deps $PWD
@@ -15,5 +15,5 @@ echo "### Installing MarkupSafe"
 pip3 install --no-index --find-links dist MarkupSafe
 
 cd ..
-rm -rf MarkupSafe-3.0.2
+rm -rf markupsafe-3.0.2
 popd
