55f543738a2287d3dfcc0dd4576cb7eb6083bad6
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2025-04-13 08:04:46 +0200

    add certs to wget

--- blfs/scripts/wget.sh
+++ blfs/scripts/wget.sh
@@ -20,5 +20,12 @@ pushd ${SOURCES}
   cd ..
   rm -rf wget-1.25.0
 popd
+
+echo "Installing certs"
+install -v -d -m755 /etc/ssl/certs
+cp -v /scripts/blfs/sources/cacert.pem /etc/ssl/certs/ca-certificates.crt
+
+echo "ca_certificate=/etc/ssl/certs/ca-certificates.crt" >> /etc/wgetrc
+
 echo "Done installing wget"
 
