6c80d35357a3d7ab77b3da256c067e104bbe932f
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2025-04-13 09:18:40 +0200

    install curl with dependencies

--- blfs/scripts/curl.sh
+++ blfs/scripts/curl.sh
@@ -5,6 +5,56 @@ SOURCES=/scripts/blfs/sources/
 
 # TODO summarize kernel build and driver install
 #
+echo "Installing libunistring"
+pushd ${SOURCES}
+  tar -xvf libunistring-1.3.tar.xz
+  cd libunistring-1.3
+
+  ./configure --prefix=/usr    \
+            --disable-static \
+            --docdir=/usr/share/doc/libunistring-1.3 &&
+  make
+  make install
+
+  cd ..
+  rm -rf libunistring-1.3
+popd
+echo "Done installing libunistring"
+
+echo "Installing libidn2"
+pushd ${SOURCES}
+  tar -xvf libidn2-2.3.7.tar.gz
+  cd libidn2-2.3.7
+
+  ./configure --prefix=/usr --disable-static &&
+
+  make
+  make install
+
+  cd ..
+  rm -rf libidn2-2.3.7
+popd
+echo "Done installing libidn2"
+
+echo "Installing libpsl"
+pushd ${SOURCES}
+  tar -xvf libpsl-0.21.5.tar.gz 
+  cd libpsl-0.21.5
+
+  mkdir build &&
+  cd build &&
+
+  meson setup --prefix=/usr --buildtype=release &&
+
+  ninja
+
+  ninja install
+
+  cd ../..
+  rm -rf libpsl-2.21.5
+popd
+echo "Done installing libpsl"
+
 echo "Installing curl"
 pushd ${SOURCES}
   tar -xvf curl-8.12.1.tar.xz
